f8bfe02905506c9f505ee249497f123f15f09122
Manu <manukumar02@gmail.com>
2021-11-03 00:28:58 +0530

    Initial Commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MarketingApp from './container/MarketingApp';
+
+const App = ()=> {
+    return (
+        <div>
+            <h1>Hi Container!</h1>
+            <hr/>
+            <MarketingApp/>
+        </div>
+    )
+}
+
+export default App;
